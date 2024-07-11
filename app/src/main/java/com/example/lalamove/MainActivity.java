5f3ef6607768500e362e1ae6fa55ef2f12b0f639
PhanMaiQuocKiet <22dh111828@st.huflit.edu.vn>
2024-07-11 14:13:13 +0700

    C database

--- app/src/main/java/com/example/lalamove/MainActivity.java
+++ app/src/main/java/com/example/lalamove/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CreateDatabase createDatabase = new CreateDatabase(this);
+        createDatabase.getWritableDatabase();
         createDatabase.close();
     }
 }
