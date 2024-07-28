02c94de190bdb12ac54399946877e12dfbcee0f2
DaoDat <daonguyendat01637@gmail.com>
2024-07-29 00:50:54 +0700

    fix merge (completed)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.navigation.ui)
 
 
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
