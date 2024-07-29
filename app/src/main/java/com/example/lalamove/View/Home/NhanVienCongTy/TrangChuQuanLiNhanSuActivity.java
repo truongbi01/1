1ae80c8d352effd31c4e93c60345203321679ad6
truongbi01 <laiphuoctruong2004@gmail.com>
2024-07-29 12:44:49 +0700

    Cap nhap chuyen huong vao trang quan li

--- app/src/main/java/com/example/lalamove/View/Home/NhanVienCongTy/TrangChuQuanLiNhanSuActivity.java
+++ app/src/main/java/com/example/lalamove/View/Home/NhanVienCongTy/TrangChuQuanLiNhanSuActivity.java
@@ -1,15 +1,27 @@
 package com.example.lalamove.View.Home.NhanVienCongTy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lalamove.R;
+import com.example.lalamove.View.Home.NhanVienCongTy.QuanLyTaiKhoan.QuanLiTaiKhoanActivity;
 
 public class TrangChuQuanLiNhanSuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_trang_chu_quan_li_nhan_su);
+        LinearLayout lnThongTinTaiKhoan = findViewById(R.id.ln_ThongTinTaiKhoan);
+        lnThongTinTaiKhoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TrangChuQuanLiNhanSuActivity.this, QuanLiTaiKhoanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
